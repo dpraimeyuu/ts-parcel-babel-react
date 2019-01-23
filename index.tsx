706c60847fb72e6e96dd020e5521f63e2ca6b111
dprai <dam.plaza@gmail.com>
2019-01-23 21:26:34 +0100

    Add initial parcel ts react babel setup

---
+++ index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+import App from "./src/App";
+
+render(
+    <App />,
+    document.getElementById("app")
+)
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default class App extends React.Component {
+    render = () => (
+        <div>
+            HELLO REACT
+        </div>
+    )
+}
