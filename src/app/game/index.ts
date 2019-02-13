75646a1c3c4fe3ca6aa83eaceff11469b948d228
dprai <dam.plaza@gmail.com>
2019-02-13 23:11:15 +0100

    Add computed part

--- src/app/game/index.ts
+++ src/app/game/index.ts
@@ -2,13 +2,15 @@ import { ModuleDefinition, Provider } from 'cerebral'
 
 import * as sequences from "./sequences/index";
 import { State } from './types'
-import { Time } from "./providers/time/index";
+import { Time } from "./providers/time";
+import { timeLeft } from './computeds/timeLeft';
 
 const state: State = {
     cards: [],
     status: "NOT_STARTED",
     currentTime: 0,
-    points: 0
+    points: 0,
+    timeLeft
 }
 
 const main: ModuleDefinition<State> = {
