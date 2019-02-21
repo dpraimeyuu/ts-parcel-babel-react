81ec27d2d082830e6f7939a1f8ba8b841a980fd0
dpraimeyuu <dam.plaza@gmail.com>
2019-02-21 15:30:49 +0100

    Fix mutation issue

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -75,7 +75,7 @@ const App: React.FunctionComponent<Dependencies & ConnectedProps> = (props) => (
             {
                 props.gameStatus === "PLAYING" && props.cards && <>
                     <TimeLeft  />
-                    <CardsGrid onClick={props.cardSelected} cards={props.cards.map(c => c)}/>
+                    <CardsGrid onClick={props.cardSelected} cards={props.cards}/>
                 </>
             }
             { props.gameStatus === "PLAYER_WIN" && <span style={{ color: "green" }}>Congratz! You've completed the game! You got: { props.points } points!</span> }
